c443e64dbe6021a27c20ebc854c6cd526d7c1f06
Devesh Varma <51313129+Devesh99@users.noreply.github.com>
2020-01-28 22:04:04 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << endl;
+
+typedef long long ll;
+const ll mod = 1000000007;
+
+#define For(i,n) for(int i=0;i<n;i++)
+
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t,i,n,j;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
